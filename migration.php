711c25cc2af762aec39fce6815c5a73b6d8060ea
Igor <isublimity@gmail.com>
2016-09-22 14:12:19 +0300

    init

---
+++ migration.php
@@ -0,0 +1,35 @@
+<?php
+include_once __DIR__ . '/phpClickHouse/include.php';
+
+$config = include_once __DIR__ . '/../_clickhouse_config_product.php';
+
+$cl = new ClickHouseDB\Cluster($config);
+
+$cl->setScanTimeOut(2.5); // 2500 ms
+if (!$cl->isReplicasIsOk())
+{
+    throw new Exception('Replica state is bad , error='.$cl->getError());
+}
+//
+
+
+$migration=include 'example/001_add_some.php';
+
+if ($migration instanceof ClickHouseDB\Cluster\Migration)
+{
+
+    $cluster_name=$migration->getClusterName();
+
+    echo "> $cluster_name , count shard   = ".$cl->getClusterCountShard($cluster_name)." ; count replica = ".$cl->getClusterCountReplica($cluster_name)."\n";
+
+    if (!$cl->sendMigration($mclq,true))
+    {
+        throw new Exception('sendMigration is bad , error='.$cl->getError());
+    }
+    echo "All ok!\nExit;)\n";
+    exit(0);
+}
+else
+{
+    throw new Exception("migration not load");
+}
