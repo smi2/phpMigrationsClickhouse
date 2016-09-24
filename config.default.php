b0224c004d17314492443e5671b7ba767391cab8
Igor <isublimity@gmail.com>
2016-09-24 15:01:00 +0300

    init

--- config.default.php
+++ config.default.php
@@ -4,7 +4,8 @@ return
     'clickhouse.server.1.migrations'=>
         [
             'clickhouse'=>['host'=>'x.x.net','port'=>'8123','username'=>'x','password'=>'x'],
-            'repository'=>__DIR__ . '/../ClickHouseMigrations/x1x/',
+            'repository'=>__DIR__ . '/../ClickhouseMigrations/',
+            'path'=>'ch.cluster1',
             'split'=>['query'=>';;'],
             'coordinator'=>['pdo'=>['host'=>'']]
         ]
--- migration.php
+++ migration.php
@@ -1,22 +1,105 @@
 <?php
+
+include_once __DIR__ . '/vendor/autoload.php';
 include_once __DIR__ . '/phpClickHouse/include.php';
 // ;
 
-$config_list = include_once 'config.php';
-foreach ($config_list as $cluster_id=>$config)
+
+
+class MigrationRepo
 {
+    private $path;
+    public function __construct($repo,$path)
+    {
+        $this->path=$path;
+        $this->git = \Coyl\Git\Git::open($repo);
+        // https://github.com/cpliakas/git-wrapper
+        // https://github.com/teqneers/PHP-Stream-Wrapper-for-Git
+        // http://gitonomy.com/doc/gitlib/master/api/commit/
+        $this->openRepo();
+    }
+    private function dirMigrations()
+    {
+        return $this->git->getRepoPath()."/".$this->path;
+    }
+    private function openRepo()
+    {
+        $this->git->fetch();
+        $this->git->pull('origin', 'master');
+    }
+    private function pushRepo()
+    {
+        if ($this->git->hasChanges()) {
+            $this->git->commit('Migration done.');
+            $this->git->push('origin', 'master');
+        }
+    }
+    public function getList()
+    {
+
+    }
+    public function getNext()
+    {
+
+    }
+    public function getContent($hash)
+    {
+        //
+    }
+    public function setStart($hash)
+    {
+        $filename=$this->files[$hash]['filename'];
+
+    }
+    public function setDone($hash)
+    {
+        //
+    }
+    public function setBad($hash)
+    {
+        //
+    }
+}
+
+use PhpSchool\CliMenu\CliMenu;
+use PhpSchool\CliMenu\CliMenuBuilder;
+
+$itemCallable = function (CliMenu $menu) {
+    echo $menu->getSelectedItem()->getText();
+};
+
+$menu = (new CliMenuBuilder);
+$menu->setTitle('Clickhouse Tools, Select configuration')->addLineBreak('-');
+$menu->addItem("Show ", $itemCallable);
+
+
+
+$config_list = include_once 'config.php';
+foreach ($config_list as $cluster_id=>$config) {
     if (!isset($config['repository'])) die('xxx1');
     if (!isset($config['clickhouse'])) die('xxx2');
     if (!isset($config['split']['query'])) die('xxx2');
 
 
+    $mr = new MigrationRepo($config['repository'], $config['path']);
 
     $cl = new ClickHouseDB\Cluster($config['clickhouse']);
     $cl->setScanTimeOut(5);
-    if (!$cl->isReplicasIsOk())
-    {
-        throw new Exception('Replica state is bad , error='.$cl->getError());
+    if (!$cl->isReplicasIsOk()) {
+        throw new Exception('Replica state is bad , error=' . $cl->getError());
     }
+
+
+    $menu->addItem($cluster_id, $itemCallable);
+}
+
+
+
+$menu->build()->open();
+
+
+//
+
 
     // class repo , git pull
     // open dir
@@ -26,7 +109,6 @@ foreach ($config_list as $cluster_id=>$config)
     // exec migration
     // unlock coordinator
     // fun!
-}
 
 exit;
 
